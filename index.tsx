93818b5a22749166cacd6006d49c2e2c449b9163
tr013432-design <tr013432@gmail.com>
2026-01-16 18:38:07 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; 
+// 1. Importação da ferramenta do Google
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -8,8 +11,14 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. O App DEVE estar dentro deste Provider para o Dante funcionar */}
+    {/* Se você não tiver um ID real agora, pode deixar esse texto de exemplo, 
+        o Dante vai abrir, mas o botão de salvar vai dar erro ao clicar. */}
+    <GoogleOAuthProvider clientId="COLE_SEU_CLIENT_ID_DO_GOOGLE_AQUI">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
