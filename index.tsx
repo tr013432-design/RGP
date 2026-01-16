307130d205120237d744e5b648685b653df62f79
tr013432-design <tr013432@gmail.com>
2026-01-16 18:04:57 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// 1. Adicione esta linha:
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -12,9 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    {/* 2. Envolva o App com este componente e coloque seu ID */}
-    <GoogleOAuthProvider clientId="COLE_SEU_CLIENT_ID_AQUI_SE_TIVER">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
